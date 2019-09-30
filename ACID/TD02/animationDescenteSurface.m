28e3c2016544568c8514d4c504311f0a7ef09314
Adri <adrien.boitelle@gmail.com>
2019-09-30 16:11:58 +0200

    finish TD02-04

--- ACID/TD02/animationDescenteSurface.m
+++ ACID/TD02/animationDescenteSurface.m
@@ -4,9 +4,12 @@ function [] = animationDescenteSurface(pdep)
   
   X = -8 : 8;
   Y = -8 : 8;
+  
+  coeffsX = [2,3,2];
+  coeffsY = [2,7,12];
 
   Y = Y';
-  Z = func(X,Y);
+  Z = func(coeffsX,coeffsY,X,Y);
   % Z = X.^2 - 3*X + 2 + Y.^2 + 7*Y + 12;
 
   surf(X,Y,Z);
@@ -18,30 +21,37 @@ function [] = animationDescenteSurface(pdep)
   pcurr = pdep + delta;
   pprec = pdep;
 
-
-  plot3(pdep(1), pdep(2), func(pdep(1), pdep(2)), 'ob');
+  plot3(pdep(1), pdep(2), func(coeffsX,coeffsY,pdep(1), pdep(2)), 'ok');
   % axis([-8 8 -8 8]);
+      xd = valeurPolynome(derivPoly(coeffsX),pcurr(1)) * delta;
+      yd = valeurPolynome(derivPoly(coeffsY),pcurr(2)) * delta;
 
 
-  while(abs(pprec(1) - pcurr(1)) > epsilon && abs(pprec(2) - pcurr(2)) > epsilon)
+  while(xd > epsilon && yd > epsilon)
       
-      plot3(pcurr(1),pcurr(2),func(pcurr(1), pcurr(2)), 'ob');
+      plot3(pcurr(1),pcurr(2),func(coeffsX,coeffsY,pdep(1), pdep(2)), 'ob');
       pprec = pcurr;
       
-      xd = pcurr(1) * 2 - 3;
-      yd = pcurr(2) * 2 + 7;
+      xd = valeurPolynome(derivPoly(coeffsX),pcurr(1)) * delta;
+      yd = valeurPolynome(derivPoly(coeffsY),pcurr(2)) * delta;
       
       pcurr(1) += -(xd/abs(xd)) * delta;
       pcurr(2) += -(yd/abs(yd)) * delta;
       
-      delta *= pasMult;
       pause(0.001)
+      
   end
-  plot3(pcurr(1),pcurr(2),func(pcurr(1), pcurr(2)), 'ob');
+  plot3(pcurr(1),pcurr(2),func(coeffsX,coeffsY,pdep(1), pdep(2)), 'ox');
+  pcurr(1)
+  pcurr(2)
   
   hold off;
 end
 
-function [ Z ] = func(X,Y)
-  Z = (X-1).*(X-2)+(Y+3).*(Y+4);
+function [ Z ] = func(coeffX,coeffY,X,Y)
+  Z = valeurPolynome(coeffX,X) + valeurPolynome(coeffY,Y);
+end
+
+function [ Z ] = funcder(coeffX, coeffY, X,Y)
+  Z = valeurPolynome(derivPoly(coeffX),X) + valeurPolynome(derivPoly(coeffY),Y);
 end
--- ACID/TD02/valeurPolynome.m
+++ ACID/TD02/valeurPolynome.m
@@ -1,3 +1,4 @@
+# n coefficients from x^n to x^0
 function [ v ] = valeurPolynome (coeff, X)
   indexes = (length(coeff)-1:-1:0);
   v = arrayfun(@(x) sum(arrayfun(@(i) x^i * coeff(length(indexes)-i),indexes)),X);
