21ea1e49a1a2c8fa52cac971df795bf10f766b26
Adri <adrien.boitelle@gmail.com>
2020-03-10 14:07:44 +0100

    update correction

--- ArchitectureLogicielle/soldierMethodFactorySol/src/soldier/core/BehaviorSoldier.java
+++ ArchitectureLogicielle/soldierMethodFactorySol/src/soldier/core/BehaviorSoldier.java
@@ -1,10 +1,17 @@
+/**
+ * D. Auber & P. Narbel
+ * Solution TD Architecture Logicielle 2016 Université Bordeaux.
+ */
 package soldier.core;
 
 public interface BehaviorSoldier {
-	public String getName();
 	public float getHealthPoints();
+
 	public boolean alive();
+
 	public void heal();
-	public float parry(float force);  // Returns the force that the soldier did not absorb
+
+	public float parry(float force);
+
 	public float strike();
 }
